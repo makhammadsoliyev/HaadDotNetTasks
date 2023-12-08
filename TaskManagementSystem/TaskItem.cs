b5d262fc66b8359dd4f2e288d8a458c92808070d
makhammadsoliyev <makhammadsoliyev@gmail.com>
2023-12-08 10:43:42 +0500

    Implement ITask by TaskItem

---
+++ TaskManagementSystem/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TaskManagementSystem;
+
+public class TaskItem : ITask
+{
+    private int id;
+    private string description;
+    private DateTime dueDate;
+    public int TaskId { get => id; set => id = value; }
+    public string Description { get => description; set => description = value; }
+    public DateTime DueDate { get => dueDate; set => dueDate = value; }
+
+    public void DisplayTaskDetails()
+    {
+        Console.WriteLine($"TaskId: {TaskId}, Description: {Description}, Due Date: {dueDate}");
+    }
+}
