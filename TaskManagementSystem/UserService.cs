f0f55bb9aed7d17ebae52c6053c645e4581a8fd7
makhammadsoliyev <makhammadsoliyev@gmail.com>
2023-12-08 11:05:11 +0500

    Implement IUserService by UserService

---
+++ TaskManagementSystem/UserService.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TaskManagementSystem;
+
+public class UserService : IUserService
+{
+    private List<TaskItem> tasks;
+    public UserService()
+    {
+        tasks = new List<TaskItem>();
+    }
+    public void AddTask(TaskItem task)
+    {
+        tasks.Add(task);
+        Console.WriteLine("Task successfully added...");
+    }
+
+    public bool DeleteTask(int taskId)
+    {
+        return tasks.Remove(GetTask(taskId));
+    }
+
+    public TaskItem GetTask(int taskId)
+    {
+        return tasks.FirstOrDefault(task => task.TaskId == taskId);
+    }
+}
